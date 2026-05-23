d6ef0595fb04028849b55060740a3eb53dd426e6
MiwUta <2996045059@qq.com>
2026-05-23 20:13:28 +0800

    添加消息缓存功能 -- 第二版

--- 4.AI智能伴侣.py
+++ 4.AI智能伴侣.py
@@ -20,7 +20,13 @@ st.set_page_config(
 # 标题
 st.title("AI智能伴侣")
 
+# Initialization: 初始化会话信息(创建一个能缓存会话信息的列表)
+if 'messages' not in st.session_state:  #每进行一次会话都会重新执行文件,所以需要在每次会话开始时初始化一个空列表
+    st.session_state['messages'] = []   #st.session_state用来储存缓存信息
 
+# 显示历史会话信息
+for message in st.session_state['messages']:
+    st.chat_message(message['role']).write(message['content'])
 
 
 # logo
@@ -43,7 +49,7 @@ if prompt:  # 避免提示词为空时,消息框显示None
     # 显示用户输入的问题
     st.chat_message("user").write(prompt)
     print(f"----------->调用AI大模型,提示词：{prompt}")
-    
+    st.session_state['messages'].append({"role": "user", "content": prompt})     # 将用户输入的问题存储起来
 
     # 将提示词发送给大模型
     response = client.chat.completions.create(
@@ -59,3 +65,4 @@ if prompt:  # 避免提示词为空时,消息框显示None
     # 显示大模型返回的结果
     st.chat_message("assistant").write(response.choices[0].message.content)
     print(f"<-----------大模型返回的结果：{response.choices[0].message.content}")
+    st.session_state['messages'].append({"role": "assistant", "content": response.choices[0].message.content})     # 将大模型返回的结果存储起来
