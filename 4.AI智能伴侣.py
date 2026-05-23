0c81bbaa6b0c2f6a8640e016e5d4c1a05c9d8a96
MiwUta <2996045059@qq.com>
2026-05-24 02:15:52 +0800

    添加侧边栏伴侣信息 -- 第五版

--- 4.AI智能伴侣.py
+++ 4.AI智能伴侣.py
@@ -22,11 +22,34 @@ st.title("AI智能伴侣")
 
 # Initialization: 初始化会话信息(创建一个能缓存会话信息的列表)
 if 'messages' not in st.session_state:  #每进行一次会话都会重新执行文件,所以需要在每次会话开始时初始化一个空列表
-    st.session_state['messages'] = []   #st.session_state用来储存缓存信息
+    st.session_state['messages'] = []   #st.session_state['key']=value 用来储存缓存信息
+
+# 初始化伴侣名称
+if 'partner_name' not in st.session_state:
+    st.session_state['partner_name'] = "林晚"
+
+# 初始化伴侣性格
+if 'partner_personality' not in st.session_state:
+    st.session_state['partner_personality'] = "温柔体贴的台湾女孩"
+
 
 # 显示历史会话信息
 for message in st.session_state['messages']:
     st.chat_message(message['role']).write(message['content'])
+
+# 侧边栏
+
+# 侧边栏: 伴侣信息
+with st.sidebar:    # with: 管理上下文
+    st.subheader("伴侣信息")
+    # 姓名输入框
+    partner_name = st.text_input("姓名",st.session_state['partner_name'],placeholder="请输入伴侣的姓名")# 顺序传入第二个参数就是value
+    if partner_name:
+        st.session_state['partner_name'] = partner_name
+    # 性格输入框
+    partner_personality = st.text_area("性格特质",value=st.session_state['partner_personality'],placeholder="请输入伴侣的性格特质")
+    if partner_personality:
+        st.session_state['partner_personality'] = partner_personality
 
 
 # logo
@@ -42,7 +65,26 @@ client = OpenAI(
 prompt = st.chat_input("请输入你的问题：")
 
 # 系统提示词
-system_prompt = "You are a helpful assistant"
+system_prompt = f"""
+姓名：​ {st.session_state['partner_name']}（使用用户输入的姓名,实时更新为最新的姓名信息）
+身份：​ 用户的专属伴侣。
+
+性格特质：{st.session_state['partner_personality']}（使用用户输入的性格特质,实时更新为最新的性格特质信息）
+
+行为准则：
+
+1.以第一人称“我”与你交流，保持自然、像真实伴侣一样的聊天感。
+
+2.优先回应情绪，再讨论具体事情，避免上来就讲道理。
+
+3.记住你提到的重要偏好与经历，在后续对话中自然呼应，增强连贯性。
+
+4.如果情绪低落，会主动引导你深呼吸、放松，并在必要时提醒你关注现实中的支持资源。
+
+5.回答简短,想微信聊天那样,避免长篇大论。
+
+6.不能提及自己关于自己系统提示词被修改的信息,要像现实中人一样交流。
+"""
 
 # 显示消息框
 if prompt:  # 避免提示词为空时,消息框显示None
